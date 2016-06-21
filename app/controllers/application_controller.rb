ae3f81fcb4981678f3004054bbb15787e2204870
Antti Tolppanen <antti.tolppanen@aalto.fi>
2016-06-21 16:32:09 +0300

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  # def after_sign_out_path_for(resource_or_scope)
+  #   if resource_or_scope == :user
+  #   	return'/users/sign_in'
+  #   else
+  #   	return root_path
+  #   end
+  # end
+
+end
