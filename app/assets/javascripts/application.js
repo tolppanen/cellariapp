ccec67408fcfbbe1128470d9a5d26d481c6d1649
Antti Tolppanen <antti.tolppanen@aalto.fi>
2016-07-06 15:59:58 +0300

    Style fixes for the autocompletion, lots of tears were shed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,51 +42,35 @@ function showReviewBox(wineId) {
 
 
 
-  var ready;
-  ready = function() {
+var ready;
+ready = function() {
 
 
 
-var wines = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: "../wines.json"
+	var wines = new Bloodhound({
+	  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+	  queryTokenizer: Bloodhound.tokenizers.whitespace,
+	  prefetch: "../wines.json"
 
-});
+	});
 
- wines.initialize();
-console.log(wines);
-
-
-// var numbers = new Bloodhound({
-//   datumTokenizer: Bloodhound.tokenizers.obj.whitespace(' '),
-//   queryTokenizer: Bloodhound.tokenizers.whitespace,
-//   local: [
-//     { num: 'one' },
-//     { num: 'two' },
-//     { num: 'three' },
-//     { num: 'four' },
-//     { num: 'five' },
-//     { num: 'six' },
-//     { num: 'seven' },
-//     { num: 'eight' },
-//     { num: 'nine' },
-//     { num: 'ten' }
-//   ]
-// });
-
-// console.log(numbers);
-
-
-$("#prefetch .typeahead").typeahead(null, {
-  displayKey: 'name',
-  name: 'wine',
-  source: wines
-});
+	 wines.initialize();
+	console.log(wines);
 
 
 
-setInterval(function(){ console.log($('.typeahead').typeahead('val')); }, 1000);
+	$("#prefetch .typeahead").typeahead(null, {
+	  displayKey: 'name',
+	  name: 'wine',
+	  source: wines,
+	  offset: true,
+	  hint: false,
+	  highlight: true
+	}
+	).on('keyup', function($e, datum) {  // suggestion selected
+           console.log($('#prefetch .typeahead'));  
+    })
+
 
 
 };
@@ -94,29 +78,3 @@ setInterval(function(){ console.log($('.typeahead').typeahead('val')); }, 1000);
 $(document).ready(ready);
 $(document).on('page:load', ready)
 
-
-
-
-// var wines = new Bloodhound({
-//   datumTokenizer: Bloodhound.tokenizers.whitespace,
-//   queryTokenizer: Bloodhound.tokenizers.whitespace,
-//   // url points to a json file that contains an array of country names, see
-//   // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
-//   prefetch: '../wines.json'
-// });
-
-// var wines = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-//   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-//   'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-//   'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-//   'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-//   'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-//   'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-//   'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-//   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-// ];
-
-// $('#prefetch .typeahead').typeahead(null, {
-//   name: 'wines',
-//   source: wines
-// });
